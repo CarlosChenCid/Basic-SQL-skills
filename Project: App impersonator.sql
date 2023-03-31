4df96ac37fc6255d9e982f99e4f7560fa867dbc4
Carlos Chen <128018697+CarlosChenCid@users.noreply.github.com>
2023-03-31 13:41:28 -0500

    Update Project: App impersonator.sql

--- Project: App impersonator.sql
+++ Project: App impersonator.sql
@@ -1,3 +1,17 @@
+/*Project description:Think about your favorite apps, and pick one that stores
+your data- like a game that stores scores, an app that lets you post updates, etc. 
+
+Now in this project, you're going to imagine that the app stores your data 
+in a SQL database (which is pretty likely!), and write SQL statements 
+that might look like their own SQL.
+
+CREATE a table to store the data.
+INSERT a few example rows in the table.
+
+Use an UPDATE to emulate what happens when you edit data in the app.
+Use a DELETE to emulate what happens when you delete data in the app.*/
+
+
 CREATE TABLE exercise_records
     (id INTEGER PRIMARY KEY AUTOINCREMENT,
     exercise TEXT,
